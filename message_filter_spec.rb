08bd9e2bdbce2038479e91b5bdc886bba6462850
thori0908 <mr.takumi.pc.0908@gmail.com>
2015-06-30 12:40:54 +0900

    add variable bar in spec

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,6 +2,11 @@ require_relative 'message_filter'
 
 describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-   it { is_expected.to be_detect('hello from foo') }
-   it { is_expected.not_to be_detect('hello, world!') }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.not_to be_detect('hello, world!') }
+end
+
+describe MessageFilter, 'with argument "foo","bar"' do
+  subject { MessageFilter.new('foo', 'bar') }
+  it { is_expected.to be_detect('hello from bar') }
 end
